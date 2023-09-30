be1f7e3c82c30b1f8d1c1257498b69a10550293f
Silvio Traversaro <silvio@traversaro.it>
2023-09-30 13:22:11 +0200

    Modifications to compile Python part of gz-msgs10

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -1,14 +1,14 @@
-mkdir build_cxx
-cd build_cxx
+mkdir build_py
+cd build_py
 
-:: Set Python install dir to wrong directory to ensure Python files
-:: are not included in the libgz-msgs<major> package
+:: Set CMAKE_NSTALL_PREFIX to wrong directory to ensure C++ files
+:: are not included in the gz-msgs<major>-python package
 cmake ^
     -G "Ninja" ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX=%SRC_DIR%\wrong_cxx_install ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=ON ^
-    -DGZ_PYTHON_INSTALL_PATH=%SRC_DIR%\wrong_py_install ^
+    -DGZ_PYTHON_INSTALL_PATH=%SP_DIR% ^
     -DUSE_SYSTEM_PATHS_FOR_PYTHON_INSTALLATION:BOOL=ON ^
     -DPython3_EXECUTABLE:PATH=%PYTHON% ^
     -DPYTHON_EXECUTABLE:PATH=%PYTHON% ^
@@ -23,6 +23,7 @@ if errorlevel 1 exit 1
 cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
-:: Test.
-ctest -C Release -E "INTEGRATION_gz_TEST|basic_TEST"
+:: Test using pytest
+cd %SRC_DIR%\python\test
+pytest .\basic_TEST.py
 if errorlevel 1 exit 1
