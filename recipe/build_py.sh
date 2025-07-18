4861e8976a43d5a1ce92e9b3194ed6fa5f11ccf1
Silvio Traversaro <silvio@traversaro.it>
2025-07-18 13:49:04 +0200

    Update build_py.sh

--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+
+echo "========> Printing env value <=========="
+env
+echo "========> End of env value <=========="
+
 if [[ "${target_platform}" == osx-* ]]; then
     # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
     CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
